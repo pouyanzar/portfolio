b2f5f26322264d15dc8b0593d1298e403318ebe8
Pouyan Zarbafian <pouyan@Pouyans-MBP.lan>
2020-11-30 14:32:27 -0500

    again

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Menu from './Menu';
+import Aboutme from './Aboutme';
+import Projects from './Projects';
+import Footer from './Footer';
+import Skills from './Skills';
+import BackToTop from 'react-back-to-top-button'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Menu />
+      <Aboutme/>
+      <BackToTop showAt={100} speed={1500}  easing="easeInOutQuint" ><img src="/pic/top.jpg" className="top" alt="top-button"/></BackToTop>
+      <Skills/>
+      <Projects/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Projects.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import Projectviewer from './Projectviewer';
+
+class Projects extends Component{
+    state = {
+        projects:[
+            {
+                name:"ToDoList",
+                desc:"Responsive to do list app using React and Bootstrap",
+                link:"https://github.com/pouyanzar/https://project-todolist-1.netlify.app/",
+                pic:"/pic/todolist.png"
+            },
+            {
+                name:"BrainFlix",
+                desc:"Responsive cloned YouTube using React, Node.js, API functions",
+                link:"https://github.com/pouyanzar/brainflix",
+                pic:"/pic/brainflixSC.png"
+            },
+            {
+                name:"RecipeFinder",
+                desc:"Responsive receipe search engine using React, Node.js, food2fork website's APIs",
+                link:"https://github.com/pouyanzar/recipefinder",
+                pic:"/pic/receipeSC.png"
+            },
+            {
+                name: "Inventory",
+                desc:"As a member of a collaborative project, a warehouse designed with the possibility of adding, sorting, deleting branches, users, and items. The developers communicated via GitHub and Jira",
+                link:"https://github.com/pouyanzar/inventory",
+                pic:"/pic/inventorySC.png"
+            },
+            {
+                name:"Finder",
+                desc:"MySQL, Node.js, React have been used to build a search engine for truck parts with ability of filtering fields. ",
+                link:"https://github.com/pouyanzar/finderapp",
+                pic:"/pic/finderSC.png"
+            },
+            {
+                    name:"QFP",
+                    desc:"MySQL, Node.js, React have been used to build a online store for truck parts.",
+                    link:"https://bitbucket.org/Pou6858/capstone/src/master/",
+                    pic:"/pic/qfpSC.png" 
+            }
+
+        ]
+    }
+
+    
+    render(){
+
+        const {projects} = this.state;
+        return(
+            <div>
+                <section id="Projects"><h2 className="section__title">Projects</h2></section>
+                <p></p>
+                <Projectviewer projects={projects} />
+
+            </div>
+        )
+    }
+}
+
+export default Projects;
