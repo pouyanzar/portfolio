f715baa1b63efad474fb7b4c6cce2a8274eea829
Pouyan Zarbafian <pouyan@Pouyans-MBP.lan>
2020-11-30 15:07:52 -0500

    modified about me text

--- src/Aboutme.js
+++ src/Aboutme.js
@@ -12,28 +12,11 @@ class Aboutme extends Component{
                         <h2 className="section__title">About Me</h2>
                     <div className="profile1">
                         <img className="profile1__pic" src="/pic/pouyan.jpg" alt="Pouyan Zarbafian"/>
-                        {/* <p> also have completed some courses at Concordia Univeristy in Computer Science, including</p> */}
-                        {/* <ul>
-                            {this.state.skills.map((skill,i) => {
-                            return<li className="li1" key={i}>{skill}</li>
-                            })}
-                        </ul>     */}
                     </div>
-                        <p className="about__p">I am Pouyan Zarbafian, a graduate of BrainStation Full Stack Web Development Boot Camp(Fall 2018),<br/> where I learnt and deepened my understanding of the latest Web development tools and skills.</p>
+                        <p className="about__p">I am Pouyan Zarbafian, a graduate of BrainStation Full Stack Web Development Boot Camp(Fall 2018),<br/> where I learned and deepened my understanding of the latest web development tools and skills.</p>
                         <a className="button" href="/pouyan-zarbafian.pdf" download="Pouyan-Zarbafian-resume">Download my resume</a>
                     </div>
                 </section>
-                {/* <ul>At BrainStation, I also participated in some collaborative projects through which I learnt how to work with:
-                    <li>Github</li>
-                    <li>Jira</li>
-                </ul>
-                <ul>I also have completed some courses at Concordia Univeristy in Computer Science, including:
-                    <li>Java Object Programming</li>
-                    <li>Computer Organization and Assembly Language</li>
-                    <li>Priciples of Data Structures</li>
-                    <li>Operating Systems</li>
-                    <li>Files and Databases</li>
-                </ul> */}
 
             </div>
         )
