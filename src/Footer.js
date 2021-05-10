cd7b0003e1f5edac6ae44d989a77fd7aa2a7eb8f
Pouyan Zarbafian <pouyan.zarbafian@hotmail.com>
2021-05-10 06:46:12 -0400

    footer refactored

--- src/Footer.js
+++ src/Footer.js
@@ -1,20 +1,17 @@
 import React,{Component} from 'react';
 
-export default class Footer extends Component{
+export default function Footer(){
 
-    render()
-    {
-        return (
-            <div className="footer">
-                <section id="ContactMe"><h2 className="section__title"> Contact Me</h2></section>
-                <p>
-                    <a href="mailto:pouyan.zarbafian@hotmail.com">pouyan.zarbafian@hotmail.com</a>
+    return (
+        <footer className="footer">
+            <section id="ContactMe"><h2 className="section__title"> Contact Me</h2></section>
+            <p>
+                <a href="mailto:pouyan.zarbafian@hotmail.com">pouyan.zarbafian@hotmail.com</a>
+            </p>
+            <p className="footer__img">
+                <a href="https://github.com/pouyanzar"><img src="/pic/github.png" alt="GitHub"/></a>
+                <a href="https://www.linkedin.com/in/pouyan-zarbafian/"><img src="/pic/linkedin.png" alt="Linkedin"/></a>
                 </p>
-                <p className="footer__img">
-                    <a href="https://github.com/pouyanzar"><img src="/pic/github.png" alt="GitHub"/></a>
-                    <a href="https://www.linkedin.com/in/pouyan-zarbafian/"><img src="/pic/linkedin.png" alt="Linkedin"/></a>
-                    </p>
-            </div>
-        )
-    }
+        </footer>
+    )
 }
