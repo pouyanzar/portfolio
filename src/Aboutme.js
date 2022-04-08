0435ed00a4a0c7b0402cd231b7aeb91f80dc0d5a
Pouyan <pouyan.zarbafian@hotmail.com>
2022-04-08 19:51:49 -0400

    update about me

--- src/Aboutme.js
+++ src/Aboutme.js
@@ -13,7 +13,7 @@ class Aboutme extends Component{
                     <div className="profile1">
                         <img className="profile1__pic" src="/pic/pouyan.jpg" alt="Pouyan Zarbafian"/>
                     </div>
-                        <p className="about__p">I am Pouyan Zarbafian, a Computer Science graduate from Concordia Universiry, and a graduate of BrainStation College Full Stack Web Development Boot Camp(Fall 2018),<br/> where I learned and deepened my understanding of the latest web development tools and skills.</p>
+                        <p className="about__p">I am Pouyan Zarbafian, an alumni of Concordia Universiry in Computer Science, and LightHouse Labs alumni in Web development. I also graduated from BrainStation College as a Full Stack Web Developer in 2018.<br/> According to my education, I learned and deepened my understanding of the latest web development tools and skills.</p>
                         <a className="button" href="/pouyan-zarbafian.pdf" download="Pouyan-Zarbafian-resume">Download my resume</a>
                     </div>
                 </section>
