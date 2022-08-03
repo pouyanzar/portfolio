e3c2c9cff3aa49c62c86e00d7f3f9c98a29186de
Pouyan <pouyan.zarbafian@hotmail.com>
2022-08-03 11:31:17 -0400

    add to skills

--- src/Skills.js
+++ src/Skills.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React from "react";
 
-export default function Skills(){
+export default function Skills() {
+  const skills = [
+    "/pic/html51.svg",
+    "/pic/css3.svg",
+    "/pic/sass.svg",
+    "/pic/js.svg",
+    "/pic/react.png",
+    "/pic/npm.svg",
+    "/pic/nodejs3.svg",
+    "/pic/github.svg",
+    "/pic/jira.svg",
+    "/pic/sql.png",
+    "/pic/mysql.png",
+    "/pic/mongodb.png",
+    "/pic/postgresql.png",
+  ];
 
-    const skills = [
-            "/pic/html51.svg",
-            "/pic/css3.svg",
-            "/pic/sass.svg",
-            "/pic/js.svg",
-            "/pic/react.png",
-            "/pic/npm.svg",
-            "/pic/nodejs3.svg",
-            "/pic/github.svg",
-            "/pic/jira.svg"
-        ]
-
-    return(
-        <section id="Skills" className="skill">
-            <h2 className="section__title">Skills</h2>
-            <div className="skill__img">{skills.map((skill,index)=>{
-                return  <img src={skill} className="skill__img--pic" key={index} alt={skill}/>
-                })}
-            </div> 
-        </section>
-    )
+  return (
+    <section id="Skills" className="skill">
+      <h2 className="section__title">Skills</h2>
+      <div className="skill__img">
+        {skills.map((skill, index) => {
+          return (
+            <img
+              src={skill}
+              className="skill__img--pic"
+              key={index}
+              alt={skill}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
 }
