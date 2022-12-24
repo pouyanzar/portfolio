9629e92da9aaba14d24d85061c299e6cce5b905e
Pouyan <pouyan.zarbafian@hotmail.com>
2022-12-24 01:32:08 -0500

    add robofriends project

--- src/Projects.js
+++ src/Projects.js
@@ -5,6 +5,14 @@ function Projects() {
   const projects = [
     {
       id: 1,
+      name: "RoboFriends",
+      desc: "Responsive search engine using React Hooks, Tachyons and Redux to filter robots",
+      link: "https://pouyanzar.github.io/robofriends",
+      github: "https://github.com/pouyanzar/robofriends",
+      pic: "/pic/robofriends.png",
+    },
+    {
+      id: 2,
       name: "ToDoList",
       desc: "Responsive ToDoList app using React Hooks and Bootstrap",
       link: "https://project-todolist-1.netlify.app/",
@@ -12,35 +20,35 @@ function Projects() {
     },
 
     {
-      id: 2,
+      id: 3,
       name: "BrainFlix",
       desc: "Responsive cloned YouTube using React, Node.js, API functions",
       link: "https://github.com/pouyanzar/brainflix",
       pic: "/pic/brainflixSC.png",
     },
     {
-      id: 3,
+      id: 4,
       name: "FoodFinder",
       desc: "Responsive receipe search engine using React, Node.js, edamam website's APIs",
       link: "https://modest-booth-b055ca.netlify.app/",
       pic: "/pic/receipeSC.png",
     },
     {
-      id: 4,
+      id: 5,
       name: "Inventory",
       desc: "As a member of a collaborative project, a warehouse designed with the possibility of adding, sorting, deleting branches, users, and items. The developers communicated via GitHub and Jira",
       link: "https://inventap.herokuapp.com",
       pic: "/pic/inventorySC.png",
     },
     {
-      id: 5,
+      id: 6,
       name: "Finder",
       desc: "MySQL, Node.js, React have been used to build a search engine for truck parts with ability of filtering fields. ",
       link: "https://github.com/pouyanzar/finderapp",
       pic: "/pic/finderSC.png",
     },
     {
-      id: 6,
+      id: 7,
       name: "QFP",
       desc: "MySQL, Node.js, React have been used to build a online store for truck parts.",
       link: "https://github.com/pouyanzar/qfp",
@@ -51,7 +59,6 @@ function Projects() {
   return (
     <section id="Projects">
       <h2 className="section__title">Projects</h2>
-      {/* <p></p> */}
       <Projectviewer projects={projects} />
     </section>
   );
--- src/Projectviewer.js
+++ src/Projectviewer.js
@@ -4,12 +4,17 @@ function Projectviewer({ projects }) {
       <article className="project" key={index} id={project.name}>
         <h2 className="project__h2">{project.name}</h2>
         <p className="project__p">{project.desc}</p>
-        <div className="Project__ImgWrapper">
+        <div className="project__ImgWrapper">
           <img className="project__img" src={project.pic} alt={project.name} />
         </div>
         <a href={project.link} target="_blank" rel="noopener noreferrer">
           <button className="project__button" type="button" value="View">
             View
+          </button>
+        </a>
+        <a href={project.github} target="_blank" rel="noopener noreferrer">
+          <button className="project__button" type="button" value="GitHub">
+            GitHub
           </button>
         </a>
       </article>
